fa5884f4f81a883daafb80c5206b3048d8406713
littlefxc <2575974990@qq.com>
2019-01-17 17:32:18 +0800

    插入排序 todo

--- src/main/java/com/littlefxc/examples/algorithm/InsertionSort.java
+++ src/main/java/com/littlefxc/examples/algorithm/InsertionSort.java
@@ -1,9 +1,35 @@
 package com.littlefxc.examples.algorithm;
 
 /**
- * 插入排序
+ * 插入排序 TODO
+ *
  * @author fengxuechao
  * @date 2019/1/17
  **/
 public class InsertionSort {
+
+    private InsertionSort() {
+    }
+
+    public static void sort(Comparable[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j > 0; j++) {
+
+            }
+        }
+    }
+
+    /**
+     * 交换元素在数组的中位置
+     *
+     * @param arr 被交换元素所在的数组
+     * @param i   被交换元素i的索引
+     * @param j   被交换元素j的索引
+     */
+    private static void swap(Object[] arr, int i, int j) {
+        Object objTmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = objTmp;
+    }
 }
