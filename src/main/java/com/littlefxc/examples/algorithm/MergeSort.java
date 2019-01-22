2a5f99b5a2b69424f68bba995814b421fbacab2e
littlefxc <12741723+littlefxc@users.noreply.github.com>
2019-01-22 13:36:22 +0800

    归并排序优化及测试

--- src/main/java/com/littlefxc/examples/algorithm/MergeSort.java
+++ src/main/java/com/littlefxc/examples/algorithm/MergeSort.java
@@ -17,12 +17,20 @@ public class MergeSort {
      * @param arr
      * @param left  被排序数组的第一个索引
      * @param right 被排序数组的最后一个索引
-     * @version 2
+     * @version 3
      */
     private static void sort(Comparable[] arr, int left, int right) {
-        if (left >= right) {
+        /*if (left >= right) {
+            return;
+        }*/
+
+        // 当arr的容量小到一定程度的时候，使用插入排序会更好, todo n 取多少值时更合适？
+        int n = 15;
+        if (right - left <= n) {
+            insertionSort(arr, left, right);
             return;
         }
+
         // todo 当left + right > Integer.MAX_VALUE时, 发生数据溢出情况
         int midIndex = (left + right) / 2;
         sort(arr, left, midIndex);
@@ -30,6 +38,25 @@ public class MergeSort {
         if (arr[midIndex].compareTo(arr[midIndex + 1]) > 0) {
             // 当arr[midIndex].compareTo(arr[midIndex + 1]) > 0时，需要排序
             merge(arr, left, midIndex, right);
+        }
+    }
+
+    /**
+     * 插入排序
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    private static void insertionSort(Comparable[] arr, int left, int right) {
+        for( int i = left + 1 ; i <= right ; i ++ ){
+            Comparable key = arr[i];
+            int j = i - 1;
+            while (j > left && arr[j].compareTo(key) > 0) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j] = key;
         }
     }
 
